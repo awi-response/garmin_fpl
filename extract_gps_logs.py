b6a81afd13988ea17c9c33cbfd7669eac327ff5e
Ingmar Nitze <initze@dmawi.de>
2023-07-07 21:49:19 -0600

    improved style

--- extract_gps_logs.py
+++ extract_gps_logs.py
@@ -1,67 +1,48 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[29]:
-
-
-import geopandas as gpd
-from pathlib import Path
-import pandas as pd
-from shapely.geometry import Point, LineString
 import sys
+from pathlib import Path
+
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point, LineString
 
 
-# In[3]:
+def main():
+    # Read the command line argument as the GPS directory
+    if len(sys.argv) < 2:
+        print("Please provide the GPS directory path as a command line argument.")
+        return
+
+    gps_directory = Path(sys.argv[1])
+
+    # Path to the actual track file
+    actual_track_file = gps_directory / 'ActualTrack.dat'
+
+    # Read the actual track data into a DataFrame
+    df = pd.read_table(actual_track_file, header=None, names=['lon', 'lat', 'altitude'])
+    df = df.drop_duplicates()
+
+    # Create Point geometries from lon and lat columns
+    geometry = [Point(xy) for xy in zip(df['lon'], df['lat'])]
+
+    # Create a GeoDataFrame from the DataFrame and Point geometries
+    gdf = gpd.GeoDataFrame(df, geometry=geometry, crs='EPSG:4326')
+
+    # Create a LineString from the Point geometries
+    line = LineString(geometry)
+
+    # Create a GeoSeries with the LineString
+    line_series = gpd.GeoSeries([line])
+
+    # Create a new GeoDataFrame with the LineString geometry
+    line_gdf = gpd.GeoDataFrame(geometry=line_series, crs=gdf.crs)
+
+    # Save the GeoDataFrames to GeoJSON files
+    gdf.to_file(gps_directory / 'track.geojson')
+    line_gdf.to_file(gps_directory / 'track_line.geojson')
 
 
-#GPSDIR = Path('GPS_070723')
-GPSDIR = Path(sys.argv[1])
-
-
-# In[5]:
-
-
-actual_track = GPSDIR / 'ActualTrack.dat'
-
-
-# In[14]:
-
-
-df = pd.read_table(actual_track, header=None, names=['lon','lat', 'altitude'])
-df = df.drop_duplicates()
-
-
-# In[19]:
-
-
-geometry = [Point(xy) for xy in zip(df['lon'], df['lat'])]
-
-# Create the GeoDataFrame
-gdf = gpd.GeoDataFrame(df, geometry=geometry, crs='EPSG:4326')
-
-
-# In[24]:
-
-
-# Create a LineString from the points
-line = LineString(geometry)
-
-# Create a GeoSeries with the LineString
-line_series = gpd.GeoSeries([line])
-
-# Create a new GeoDataFrame with the LineString geometry
-line_gdf = gpd.GeoDataFrame(geometry=line_series, crs=gdf.crs)
-
-
-# In[27]:
-
-
-gdf.to_file(GPSDIR / 'track.geojson')
-line_gdf.to_file(GPSDIR / 'track_line.geojson')
-
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    main()
