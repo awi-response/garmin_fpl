98957d2dc9544c92932a0572f1949bc1584a1cc9
Vrodron <141745019+Vrodron@users.noreply.github.com>
2023-09-14 11:21:52 +0200

    Update 03_BBox_creator.py

--- 03_BBox_creator.py
+++ 03_BBox_creator.py
@@ -4,15 +4,19 @@ import sys
 import glob
 import numpy as np
 
-def decimal_degrees_to_dmm(decimal_degrees):
-    # function to calculate decimal degrees to decimal mintes
+def decimal_degrees_to_ddm(decimal_degrees):
+    '''
+     function to calculate decimal degrees to decimal minutes
+     '''
     degrees = int(decimal_degrees) # get degrees
     decimal_minutes = (decimal_degrees - degrees) * 60 # convert minutes to decimal minutes
     decimal_minutes = np.round(decimal_minutes, decimals=1) # round to format necessary for gfp format (1 decimal)
     return degrees, decimal_minutes # get degree integer and decimal minutes
 
 def convert2stringLon(val):
-    # function to convert Longitude to string with E/W indicator and d/m as deg and minute sign
+    '''
+     function to convert Longitude to string with E/W indicator and d/m as deg and minute sign
+    '''
     deg = val[0]
     min = val[1]
     if deg<0:
@@ -45,16 +49,16 @@ def BBoxCeator(fname):
                 # minLat, maxLat, minLon, maxLon
 
 
-
-    # Latitude: each minute is approx 1 mile: [+2,-2] for minutes of maxLat and minLat
-    # Longitute at Lat 68: apporximately 3 minutes correspond to 2 km --> [+3,-3] for minutes of maxLon, minLon
-
-    cornersDMM = list(map(decimal_degrees_to_dmm, corners))
+    '''
+    Latitude: each minute is approx 1 mile: [+2,-2] for minutes of maxLat and minLat
+    Longitute at Lat 68: apporximately 3 minutes correspond to 2 km --> [+3,-3] for minutes of maxLon, minLon
+    '''
+    cornersDDM = list(map(decimal_degrees_to_ddm, corners))
     bbox_enhancers = [-2,2,-3,+3]
 
     for i in range(0, len(bbox_enhancers)):
-        deg = cornersDMM[i][0]
-        minu = cornersDMM[i][1]+bbox_enhancers[i]
+        deg = cornersDDM[i][0]
+        minu = cornersDDM[i][1]+bbox_enhancers[i]
         if abs(minu) > 60:
             print(minu)
             print(int(np.round(minu/60,decimals=0)))
@@ -62,14 +66,14 @@ def BBoxCeator(fname):
             print(deg)
             minu = np.round(abs(minu) % 60,decimals=1)
             print(minu)
-        cornersDMM[i] = (deg,minu)
+        cornersDDM[i] = (deg,minu)
 
     # combine to ll,ul,lr,ur
 
-    ll = cornersDMM[0],cornersDMM[2] #Latmin + Lonmin
-    ul = cornersDMM[1],cornersDMM[2] #Latmax + Lonmin
-    lr = cornersDMM[0],cornersDMM[3] #
-    ur = cornersDMM[1],cornersDMM[3]
+    ll = cornersDDM[0],cornersDDM[2] #Latmin + Lonmin
+    ul = cornersDDM[1],cornersDDM[2] #Latmax + Lonmin
+    lr = cornersDDM[0],cornersDDM[3] #
+    ur = cornersDDM[1],cornersDDM[3]
 
     bbox = pd.DataFrame(data = [ll,ul,lr,ur])
     
@@ -80,8 +84,7 @@ def BBoxCeator(fname):
 
 
 if __name__ == '__main__':
-    fnames = glob.glob(f'{sys.argv[1]}\*_renamed.wpt')#'/mnt/c/Users/vedoep001/Documents/03__Expeditions/user.wpt'#sys.argv[1]
-    #fnames = glob.glob('/mnt/c/Users/vedoep001/Documents/03__Expeditions/07__Flightplanning/03_dailyFLPs/2023-07-05/*_renamed.wpt')
+    fnames = glob.glob(f'{sys.argv[1]}\*_renamed.wpt')
     print(f'{sys.argv[1]}')
     print(fnames)
     list(map(BBoxCeator,fnames))
