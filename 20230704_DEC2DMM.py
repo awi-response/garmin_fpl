314023799dc2f6f60fa86ee2fffa6ab09c70237f
Vrodron <141745019+Vrodron@users.noreply.github.com>
2023-09-14 10:59:21 +0200

    Update 20230704_DEC2DMM.py

--- 20230704_DEC2DMM.py
+++ 20230704_DEC2DMM.py
@@ -5,15 +5,21 @@ import glob
 
 import sys
 
-def decimal_degrees_to_dmm(decimal_degrees):
-    # function to calculate decimal degrees to decimal mintes
+def decimal_degrees_to_ddm(decimal_degrees):
+    '''
+    function to convert decimal degrees to decimal minutes
+    input: decimal degree
+    output: degree, decimal minutes
+    '''
     degrees = int(decimal_degrees) # get degrees
     decimal_minutes = (decimal_degrees - degrees) * 60 # convert minutes to decimal minutes
     decimal_minutes = np.round(decimal_minutes, decimals=1) # round to format necessary for gfp format (1 decimal)
     return degrees, decimal_minutes # get degree integer and decimal minutes
 
 def convert2stringLon(val):
-    # function to convert Longitude to string with E/W indicator and d/m as deg and minute sign
+    '''
+    function to convert Longitude to string with E/W indicator and d/m as deg and minute sign
+    '''
     deg = val[0]
     min = val[1]
     if deg<0:
@@ -25,7 +31,9 @@ def convert2stringLon(val):
     return('{:1}{:02d}d{:04.1f}m'.format(EastWest,deg,min))
 
 def convert2stringLat(val):
-    #function to convert Latitude to string with S/N indicator and d/m as deg and minute sign
+    '''
+    function to convert Latitude to string with S/N indicator and d/m as deg and minute sign
+    '''
     deg = val[0]
     min = val[1]
     if deg<0:
@@ -37,19 +45,21 @@ def convert2stringLat(val):
     return('{:1}{:02d}d{:04.1f}m'.format(NorthSouth,deg,min))
 
 def dec2dmm(fname):
-    # function to read in wpt file as exported by MACS, converts DECDeg to DMM, and save it back
-    # output remains same in overall format, only DECDeg will be in DMM
+    '''
+    function to read in wpt file as exported by MACS, converts DECDeg to DMM, and saves it back
+    output: Same as input with DECDeg will be in DDM
+    '''
     f = pd.read_csv(fname, header=None)
 
-    lat = list(map(decimal_degrees_to_dmm, f.iloc[:,2]))
-    lon = list(map(decimal_degrees_to_dmm, f.iloc[:,3]))
+    lat = list(map(decimal_degrees_to_ddm, f.iloc[:,2]))
+    lon = list(map(decimal_degrees_to_ddm, f.iloc[:,3]))
 
     lon = list(map(convert2stringLon,lon))
     lat = list(map(convert2stringLat,lat))
     f[2] = lat
     f[3] = lon
 
-    f.to_csv(fname[:-4]+'_DMM.wpt',header=False,index = False) # saves file with extension DMM
+    f.to_csv(fname[:-4]+'_DDM.wpt',header=False,index = False) # saves file with extension DDM
 
 if __name__ == '__main__':
     fname = sys.argv[1]
