c317ccf73a82ca909423a252e403b74690eb10fc
Vrodron <141745019+Vrodron@users.noreply.github.com>
2023-09-15 11:50:02 +0200

    Update 02_CombineWPT.py

--- 02_CombineWPT.py
+++ 02_CombineWPT.py
@@ -5,14 +5,14 @@ import os
 import sys
 
 def combineWPT(directory):
-'''
-combines all iii_sitename_user_renamed.wpt files within a directory to one final user.wpt.
-This list is the import file for the Garmin for importing user.wpt. This information containes the names for specific coordinates (here in decimal degree format)
-The iii_sitename_fpl.gfp file only containes the sequence of the different coordinates and extracts their names from this final user.wpt file
-The user.wpt file will be stored in the same folder as all iii_sitename_user.wpt
-input: path to directory of all relevant iii_sitename_user_renamed.wpt of the day
-output: user.wpt
-'''
+    '''
+    combines all iii_sitename_user_renamed.wpt files within a directory to one final user.wpt.
+    This list is the import file for the Garmin for importing user.wpt. This information containes the names for specific coordinates (here in decimal degree format)
+    The iii_sitename_fpl.gfp file only containes the sequence of the different coordinates and extracts their names from this final user.wpt file
+    The user.wpt file will be stored in the same folder as all iii_sitename_user.wpt
+    input: path to directory of all relevant iii_sitename_user_renamed.wpt of the day
+    output: user.wpt
+    '''
     wpts = glob.glob(os.path.join(directory,'*_renamed.wpt'))
 
     def get_df():
